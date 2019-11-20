a558f1cbb14d6f00319e1d77920b15dfd6828efd
bjh9750 <bjh9750@naver.com>
2019-11-20 20:32:33 +0900

    update source file

--- HW3/lu_decomp_mpi.cpp
+++ HW3/lu_decomp_mpi.cpp
@@ -1,62 +1,87 @@
 #include <cstdio>
 #include <cstdlib>
-#include <mpi.h>
 #include <omp.h>
+#include <mpi.h>
 #include <vector>
 #include <algorithm>
 #include <functional>
-
 using namespace std;
 
 #define ROOT 0
 #define ADDR(A, N, X, Y) &A[X*N + Y]
 
-void decomp(double *LU, int n);
-void row_calculation(double *A_pivot, double *A_target, double *LU_pivot, int size);
+void decomp(double *U, int n, int rank, int size);
+void row_calculation(double *A_pivot, double *A_target, int size, int rank);
 double verify(double *A , double *LU);
 void matrix_print(double *mat, int size);
 
+void print_row(double *A, int size, int rank, int sender){
+    if(rank !=sender) printf("rank %d sender %d", rank, sender);
+    else printf("rank %d calculate", rank);
+    for(int i = 0; i < size; ++i){
+        printf("%6.3lf ", A[i]);
+    }
+    printf("\n");
+}
+
 int main(int argc, char** argv){
     int n, s;
-    double *A, *LU;
+    double *A, *U;
+    int rank, size;
     n = atoi(argv[1]);
     s = atoi(argv[2]);
 
     srand(s);
     A = (double *)malloc(sizeof(double) * n * n);
-    LU = (double *)malloc(sizeof(double) * n * n);
+    U = (double *)malloc(sizeof(double) * n * n);
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 
     for(int i = 0; i < n; ++i){
         for(int j = 0; j < n; ++j){
             A[i*n + j] = (double) (rand() % 100);
-            LU[i*n + j] = A[i*n + j];
+            U[i*n + j] = A[i*n + j];
         }
     }
+
     
-    decomp(LU, n);
-    matrix_print(A, n);
-    matrix_print(LU, n);
-    verify(A, LU);
+    decomp(U, n, rank, size);
+
+    if(rank == 0){
+        matrix_print(A, n);
+        matrix_print(U, n);
+    }
+
+    verify(A, U);
+    MPI_Finalize();
 }
 
-void decomp(double *LU, int n){
-
-    int size, rank;
+void decomp(double *U, int n, int rank, int size){
     for(int pivot = 0; pivot < n - 1; ++pivot){
-        double *LU_pivot = ADDR(LU, n, pivot, pivot);
+        double *U_pivot = ADDR(U, n, pivot, pivot);
         for(int next_p = pivot+1; next_p < n; ++next_p){
-            double *LU_target = ADDR(LU, n, next_p, pivot);
-            row_calculation(LU_pivot, LU_target, LU_target, n - pivot);
-            //matrix_print(LU, n);
+            if(next_p % size == rank){
+                double *U_target = ADDR(U, n, next_p, pivot);
+                row_calculation(U_pivot, U_target, n - pivot, rank);
+            }
+        }
+        for(int next_p = pivot+1; next_p < n; ++next_p){
+            double *U_target = ADDR(U, n, next_p, pivot);
+            MPI_Bcast(U_target, n - pivot, MPI_DOUBLE, next_p % size, MPI_COMM_WORLD);
         }
     }
 }
 
-void row_calculation(double *A_pivot, double *A_target, double *LU_pivot, int size){
+void row_calculation(double *A_pivot, double *A_target, int size, int rank){
+    //print_row(A_target, size, rank, rank);
+    if(A_target[0] == 0) return;
     double coeff = A_target[0] / A_pivot[0];
-    LU_pivot[0] = coeff;
+    A_target[0] = coeff;
     for(int i = 1; i < size; ++i){
-        LU_pivot[i] = A_target[i] - coeff * A_pivot[i];
+        A_target[i] = A_target[i] - coeff * A_pivot[i];
     }
 }
 
